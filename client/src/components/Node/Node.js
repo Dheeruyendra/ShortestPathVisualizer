d8422da46c227e0e1e3d56777cb2c9fab516fd53
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2023-11-11 16:53:32 +0530

    Update Node.js

--- client/src/components/Node/Node.js
+++ client/src/components/Node/Node.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Node.css';
 
-const Node = ({col, row, isStart, isFinish, isWall, onMouseDown, onMouseEnter, onMouseUp}) => {
+const Node = ({ col, row, isStart, isFinish, isWall, onMouseDown, onMouseEnter, onMouseUp }) => {
     const extraClassName = isFinish ? 'node-finish' : isStart ? 'node-start' : isWall ? 'node-wall' : '';
 
+    // Updated to pass node type information in mouse event handlers
     return (
-    <div
-        id = {`node-${row}-${col}`}
-        className = {`node ${extraClassName}`}
-        onMouseDown={() => onMouseDown(col, row)}
-        onMouseEnter = {() => onMouseEnter(col, row)}
-        onMouseUp = {() => onMouseUp()}
-    ></div>
+        <div
+            id={`node-${row}-${col}`}
+            className={`node ${extraClassName}`}
+            onMouseDown={() => onMouseDown(row, col, isStart, isFinish)}
+            onMouseEnter={() => onMouseEnter(row, col, isStart, isFinish)}
+            onMouseUp={() => onMouseUp()}
+        ></div>
     );
-
 };
 
-export default Node;  
+export default Node;
